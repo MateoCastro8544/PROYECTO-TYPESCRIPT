8808d47dcb93b42b18d873d39d7c056a0cad3e6c
Mateo Castro <davidxdcastro@gmail.com>
2022-12-19 21:36:56 -0500

    cambios

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//Declaramos los arrays vacios para llenarlos espues 
 let students = [];
 let teachers = [];
+let activities = [];
+let courses = [];
+//crea una lista quemada en el input Area de Teacher
+var Area;
+(function (Area) {
+    Area["Desarrollo"] = "Desarrollo de Software";
+    Area["Marketing"] = "Marketing";
+    Area["Turismo"] = "Turismo";
+    Area["Dise\u00F1o"] = "Dise\u00F1o de Modas";
+})(Area || (Area = {}));
+// Funcion para agregar los estudiantes
 function addStudent() {
     let currentStudent = {
         fullName: readFormHtml("nameStudent"),
@@ -14,6 +26,7 @@ function addStudent() {
     students.push(currentStudent);
     console.table(students);
 }
+//funcion para agregar los profesores
 function addTeacher() {
     let currentTeacher = {
         fullName: readFormHtml("nameTeacher"),
@@ -26,6 +39,53 @@ function addTeacher() {
     teachers.push(currentTeacher);
     console.table(teachers);
 }
+function addActivity() {
+    let currentActivity = {
+        activityName: readFormHtml("activity")
+    };
+    activities.push(currentActivity);
+    console.table(activities);
+    initSelect();
+}
+function addCourses() {
+    let currentCourses = {
+        courseName: readFormHtml('course')
+    };
+    courses.push(currentCourses);
+    console.table(courses);
+    initSelect();
+}
+//sirve para pasar los datos quemados a los input select 
+function initSelect() {
+    let area = document.getElementById("area");
+    let areas = Object.values(Area);
+    areas.forEach((value) => {
+        let option = document.createElement("option");
+        option.value = value;
+        option.text = value;
+        area.add(option);
+    });
+    let courseGradeBook = document.getElementById("course-GradeBook");
+    document.querySelectorAll("#course-GradeBook option").forEach(option => option.remove());
+    courses.forEach((value) => {
+        let option = document.createElement("option");
+        option.value = value.courseName;
+        option.text = value.courseName,
+            courseGradeBook.add(option);
+    });
+    let activityGradeBook = document.getElementById("activity-GradeBook");
+    document.querySelectorAll("#activity-GradeBook option").forEach(option => option.remove());
+    activities.forEach((value) => {
+        let option = document.createElement("option");
+        option.value = value.activityName;
+        option.text = value.activityName,
+            activityGradeBook.add(option);
+    });
+}
+/*
+funcion para optimizar la lectura de los input del html y luego pasar
+a las funciones de estuduante y profesor  */
 function readFormHtml(id) {
     return document.getElementById(id).value;
 }
+initSelect();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import { Activity } from "./enties/activity";
+import { Course } from "./enties/course";
 import { Student } from "./enties/student";
 import { Teacher } from "./enties/teacher";
 
 //Declaramos los arrays vacios para llenarlos espues 
 let students : Student[] = [];
 let teachers : Teacher[] = [];
+let activities : Activity[] = [];
+let courses : Course [] = [];
 
+//crea una lista quemada en el input Area de Teacher
+enum Area{
+    Desarrollo = "Desarrollo de Software",
+    Marketing = "Marketing",
+    Turismo = "Turismo",
+    Diseño = "Diseño de Modas"
+}
 
 // Funcion para agregar los estudiantes
 function addStudent():void{
@@ -35,10 +46,77 @@ function addTeacher():void{
     console.table(teachers);
 }
 
+
+function addActivity():void {
+    let currentActivity : Activity = {
+        activityName : readFormHtml ("activity") 
+    }
+
+    activities.push(currentActivity)
+    console.table(activities);
+    initSelect()
+}
+
+function addCourses():void {
+    let currentCourses : Course = {
+        courseName : readFormHtml ('course')
+    }
+
+    courses.push(currentCourses);
+    console.table(courses);
+    initSelect()
+}
+
+
+
+
+
+
+
+
+//sirve para pasar los datos quemados a los input select 
+function initSelect():void {
+    let area = document.getElementById("area") as HTMLSelectElement;
+    let areas = Object.values(Area)
+    areas.forEach(
+        (value) => {
+            let option = document.createElement("option");
+            option.value = value;
+            option.text = value;
+            area.add(option)
+        }
+    );
+
+    let courseGradeBook = document.getElementById("course-GradeBook") as HTMLSelectElement;
+    document.querySelectorAll("#course-GradeBook option").forEach(option => option.remove())   
+courses.forEach(
+    (value) => {
+     let option = document.createElement("option"); 
+       option.value = value.courseName;
+       option.text = value.courseName,
+       courseGradeBook.add(option);
+    }
+);
+
+    let activityGradeBook = document.getElementById("activity-GradeBook") as HTMLSelectElement;
+    document.querySelectorAll("#activity-GradeBook option").forEach(option => option.remove())   
+    activities.forEach(
+    (value) => {
+    let option = document.createElement("option"); 
+    option.value = value.activityName;
+    option.text = value.activityName,
+    activityGradeBook.add(option);
+    }
+);
+}
+
 /* 
-//funcion para optimizar la lectura de los input del html y luego pasar
-//a las funciones de estuduante y profesor  */
+funcion para optimizar la lectura de los input del html y luego pasar
+a las funciones de estuduante y profesor  */
 function readFormHtml(id:string):string{
     return (<HTMLInputElement>document.getElementById(id))!.value;
 }
+
+
+initSelect()
 
